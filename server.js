4c211e204db3c5115b68e9e199fcd9e4d3bf5144
Manuel André Bottiglieri <m.bottiglieri@l22.it>
2023-02-06 12:55:52 +0100

    env error hendling

--- server.js
+++ server.js
@@ -4,10 +4,17 @@ const session = require('cookie-session');
 
 const PORT = process.env.PORT || 3000;
 const { SERVER_SESSION_SECRET, FORGE_APP_NAME, FORGE_CLIENT_ID, FORGE_CLIENT_SECRET, HOST_URL, CLI_CONFIG_PASSWORD } = process.env;
-if (!SERVER_SESSION_SECRET || !FORGE_APP_NAME || !FORGE_CLIENT_ID || !FORGE_CLIENT_SECRET || !HOST_URL || !CLI_CONFIG_PASSWORD) {
-    console.error('Some of the following env. variables are missing:');
-    console.error('SERVER_SESSION_SECRET, FORGE_APP_NAME, FORGE_CLIENT_ID, FORGE_CLIENT_SECRET, HOST_URL, CLI_CONFIG_PASSWORD');
-    return;
+try{
+    if (!SERVER_SESSION_SECRET) throw new Error('Unable to find environment variable SERVER_SESSION_SECRET')
+    if (!FORGE_APP_NAME) throw new Error('Unable to find environment variable FORGE_APP_NAME')
+    if (!FORGE_CLIENT_ID) throw new Error('Unable to find environment variable FORGE_CLIENT_ID')
+    if (!FORGE_CLIENT_SECRET) throw new Error('Unable to find environment variable FORGE_CLIENT_SECRET')
+    if (!HOST_URL) throw new Error('Unable to find environment variable HOST_URL')
+    if (!CLI_CONFIG_PASSWORD) throw new Error('Unable to find environment variable CLI_CONFIG_PASSWORD')
+    if (!PORT) throw new Error('Unable to find environment variable PORT')
+} catch (error) {
+    console.error(error)
+    return
 }
 
 let app = express();
